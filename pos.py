15b90d465e9ca8cfb8b90cfdad150782ffc3f515
Rabia <rabia.asif@mail.utoronto.ca>
2021-11-18 00:16:26 -0500

    update post req

--- pos.py
+++ pos.py
@@ -1,12 +1,16 @@
-from flask import Flask
+from flask import request, Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://nytxnomc:i-ilLZpUOx26pNnbh8ZBm0mBjBRdc0xO@fanny.db.elephantsql.com/nytxnomc'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-# app.secret_key = 'secret string'
+POSTGRES = {
+    'user': 'nytxnomc',
+    'pw': 'i-ilLZpUOx26pNnbh8ZBm0mBjBRdc0xO',
+    'db': 'nytxnomc',
+    'host': 'fanny.db.elephantsql.com',
+    'port': '5432',
+}
+
 
 db = SQLAlchemy(app)
 
@@ -15,16 +19,27 @@ class Item(db.Model):
     description = db.Column(db.String(100), unique=True, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
 
-    def __init__(self, pname, color):
+    def __init__(self, description, quantity):
         self.description = description
         self.quantity = quantity
 
 @app.route('/')
 def hello():
+    print(1111)
+    print(db)
     return '<a href="/addperson"><button> Click here </button></a>'
 
-@app.route("/addperson")
+@app.route("/addperson", methods=['POST'])
 def addperson():
-    return "coming soon"
+    print(22222)
+    description = request.form["description"]
+    print(description)
+    quantity = request.form["quantity"]
+    entry = Item(description, quantity)
+    print(entry)
+    print(22222)
+    # db.session.add(entry)
+    # db.session.commit()
+    return "coming soon" + description 
 # if __name__ == '__main__':
 #     app.run()
