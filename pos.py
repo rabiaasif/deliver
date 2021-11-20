dbfe6ee566f8758540c615cf2084da21cdc0882f
Rabia <rabia.asif@mail.utoronto.ca>
2021-11-19 23:59:40 -0500

    add update by id

--- pos.py
+++ pos.py
@@ -65,7 +65,7 @@ def get_menu():
         menu_items = "<div> <h1> Menu </h1>"
         menu_items  += " <table> <tr> <th>ID</th> <th>Description</th> <th>Price</th></tr>"
         for item in Item.query.all():
-            menu_items +=  "<tr>" + "<td>"+ str(item.id) + "</td>" + "<td>" +item.description + "</td>" + "<td>" + item.price + "</td>" + "</tr>"
+            menu_items +=  "<tr>" + "<td>"+ str(item.id) + "</td>" + "<td>" +item.description + "</td>" + "<td>" + "$" + item.price + "</td>" + "</tr>"
         return menu_items + "</table> </div>"
     except:
         "Something went wrong..."
@@ -73,10 +73,28 @@ def get_menu():
 
 @app.route("/delete-item-by-id/<id>", methods=["DELETE"])
 def delete_item(id):
-    item = Item.query.filter_by(id=id).first()
     try:
+        item = Item.query.filter_by(id=id).first()
         db.session.delete(item)
         db.session.commit()
         return "deleted item: " + str(item.id) + " " + item.description
     except Exception as e:
         return "Something went wrong...perhaps this item does not exist"
+
+@app.route("/update-item-by-id/<id>", methods=["POST"])
+def update_item(id):
+    try:
+        form_to_dict = request.form.to_dict()
+        item = Item.query.filter_by(id=id).first()
+        if 'description' in form_to_dict:
+            item.description = request.form['description']
+        if 'price' in form_to_dict:
+            item.price = request.form['price']
+        if 'quantity' in form_to_dict:
+            item.quantity = request.form['quantity']
+        item = db.session.merge(item)
+        db.session.commit()
+        return "Added Item to updated: " + item.description + " quantity:" + str(item.quantity) + " price: $" + item.price
+    except Exception as e:
+        print(e)
+        return "Something went wrong... Please try again."
